1be86d9a6734075ce1a5c07053a5671d5952715e
LuisLiraC <luis_ssbm@hotmail.com>
2020-04-23 13:45:53 -0500

    first commit kittygram

---
+++ src/components/TImeline.svelte
@@ -0,0 +1,20 @@
+<script>
+  import Card from './Card.svelte'
+  export let posts = []
+</script>
+
+<style>
+  .Timeline {
+    padding: 4em 0 0 0;
+  }
+</style>
+
+<main class="Timeline">
+  <div class="Timeline-container">
+    	{#each posts as post}
+        <Card {...post} />
+      {:else}
+        <p>Loading...</p>
+      {/each}
+  </div>
+</main>
